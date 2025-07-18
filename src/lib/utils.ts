360570ee64598802dab0671b024cc6eda8905cea
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-07-18 18:54:41 +0200

    chore: add delay function to mimic network throtling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ export function formatTime(seconds: number): string {
     const formattedSecs = secs < 10 ? `0${secs}` : secs;
     return `${mins}:${formattedSecs}`;
 }
+
+export function delay(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
