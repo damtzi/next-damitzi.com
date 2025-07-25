50b538187f3eb54c2215e456925cd40afed9c2d5
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-07-25 17:41:54 +0200

    chore: Add 'home' link to navbar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import PureBlue from "../../public/images/0007fb.png";
 import { ThemeToggle } from "./theme-toggle";
+import Link from "next/link";
 
 export const Nav = () => {
     return (
         <nav className="flex items-center justify-between">
             <div className="flex items-center gap-2">
-                <Image
-                    src={PureBlue}
-                    alt="Pure blue, hex code #0007FB"
-                    className="rounded-full"
-                    width={36}
-                    height={36}
-                />
+                <Link href="/">
+                    <Image
+                        src={PureBlue}
+                        alt="Pure blue, hex code #0007FB"
+                        className="rounded-full hover:scale-110 transition-transform duration-300"
+                        width={36}
+                        height={36}
+                    />
+                </Link>
                 <div className="flex flex-col">
                     <span>Damian Tziamtzis</span>
                     <span className="text-gray-400">Frontend Engineer</span>
